4da2eddf3f3663f9062023fd99a0d6e94f0a4bc3
AfifJ <afifjamhari.id@gmail.com>
2025-07-19 09:47:52 +0700

    add google site meta

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/context/auth";
 import { Toaster } from "react-hot-toast";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,6 +14,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="LF0FMYFTasQ1EOuVe9313bUOpjgRg7O9bZEVWVmeQQ4"
+        />
+      </Head>
       <body className={inter.className}>
         <AuthProvider>
           {children}
@@ -21,8 +28,8 @@ export default function RootLayout({ children }) {
             toastOptions={{
               duration: 3000,
               style: {
-                background: '#363636',
-                color: '#fff',
+                background: "#363636",
+                color: "#fff",
               },
             }}
           />
